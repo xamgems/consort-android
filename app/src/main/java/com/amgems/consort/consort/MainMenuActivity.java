779b6b67ed1b2384752abdd71952489e2e4b85ea
Zachary Iqbal <zachary.iq@gmail.com>
2014-11-08 15:55:34 -0800

    Added Login activity

--- app/src/main/java/com/amgems/consort/consort/MainMenuActivity.java
+++ app/src/main/java/com/amgems/consort/consort/MainMenuActivity.java
@@ -5,21 +5,16 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 
-public class MainMenu extends Activity
+public class MainMenuActivity extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
     /**
@@ -138,7 +133,7 @@ public class MainMenu extends Activity
         @Override
         public void onAttach(Activity activity) {
             super.onAttach(activity);
-            ((MainMenu) activity).onSectionAttached(
+            ((MainMenuActivity) activity).onSectionAttached(
                     getArguments().getInt(ARG_SECTION_NUMBER));
         }
     }
