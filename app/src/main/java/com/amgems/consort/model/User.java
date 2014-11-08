03ddbcf4c7a1422d7e1be3cff2a022965814ffdd
shermpay <shermanpay1991@gmail.com>
2014-11-08 15:46:44 -0800

    Initial version

--- app/src/main/java/com/amgems/consort/model/User.java
+++ app/src/main/java/com/amgems/consort/model/User.java
@@ -1,23 +1,41 @@
 package com.amgems.consort.model;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-
 /**
  * @author Sherman Pay.
  * @version 0.1, 11/8/14.
  */
 public class User {
-
-    @Expose
-    @SerializedName("userName")
-    private String userName;
-
-    @Expose
-    @SerializedName("id")
+    private String user;
     private int id;
+    private int session;
 
-    @Expose
-    @SerializedName("sessionId")
-    private int sessionId;
+    public User(String user) {
+        this.user = user;
+    }
+
+    public User(String user, int id, int session) {
+        this.user = user;
+        this.id = id;
+        this.session = session;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setSession(int session) {
+        this.session = session;
+    }
+
+    public int getSession() {
+        return session;
+    }
 }
---
+++ app/src/main/java/com/amgems/consort/serve/Requestor.java
@@ -0,0 +1,19 @@
+package com.amgems.consort.serve;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.POST;
+
+/**
+ * @author Sherman Pay.
+ * @version 0.1, 11/8/14.
+ */
+public interface Requestor {
+    @FormUrlEncoded
+    @POST("/SessionServer")
+    void connectSession(@Field("user") String user, @Field("sesssion") int session,
+                        Callback<List<Integer>> callback);
+}
